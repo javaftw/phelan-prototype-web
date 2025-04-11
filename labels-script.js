25b4200fe57a9366b413b55f19df43039bd0490e
hennie-k <hennie@kartoza.com>
2025-04-11 17:23:50 +0200

    adjusts styles

--- labels-script.js
+++ labels-script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	  console.log("parsing...");
     const clone = template.content.cloneNode(true);
 	
-	formattedText = "<h2>"+label.code+"</h2><p>"+label.prodname+"</p><b>Size "+label.size+"</b><p>Order No.: TEST<br>This is a test</p>";
+	let formattedText = "<h2>"+label.code+"</h2><p>"+label.prodname+"</p><b>Size "+label.size+"</b><p>Order No.: TEST<br>This is a test</p>";
     clone.querySelector('[data-type="contents"]').innerHTML = formattedText;
 
     container.appendChild(clone);
@@ -20,16 +20,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
   document.getElementById('global-print-labels-btn').addEventListener('click', () => {
   const printContents = document.getElementById('labels-list').innerHTML;
-  const printWindow = window.open('', '', 'height=800,width=600');
-  printWindow.document.write(`
-    <html>
-      <head>
-        <title>Print Labels</title>
-        <link rel="stylesheet" href="labels-style.css">
-      </head>
-      <body>${printContents}</body>
-    </html>
-  `);
+  const printWindow = window.open('', '', 'height=600,width=900');
+  const output = `<html><head><title>Print Labels</title></head><body>${printContents}</body></html>`;
+  console.log(output);
+  printWindow.document.write(output);
   printWindow.document.close();
   printWindow.focus();
   printWindow.print();
